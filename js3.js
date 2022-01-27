a5865c02a548fceb1ea8434a8404e2b8d1bcfb1b
Gearmo <93457954+Gearmo@users.noreply.github.com>
2022-01-26 19:39:45 -0800

    Add files via upload

---
+++ js3.js
@@ -0,0 +1,14 @@
+/*
+- First, ask user to input either "rock", "paper", or "scissors" (make sure its case-sensitive for inputs like Rock, ROCK, rOcK, etc.) This will be playerPlay
+- Create "computerPlay" function that will generate number between 1 and 3. (Make sure to return these results, or console.log if testing)
+- Based the random number generated, create "if" statements to determine winner of round.
+
+*/
+
+function playRound(playerSelection, computerSelection) {
+    const computerPlay(math.random(3));
+  }
+  
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
